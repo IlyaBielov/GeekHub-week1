d4ee868795620526ca4039c5bd901364ace89b6a
IlyaBielov <ibioriginalibi@gmail.com>
2018-12-29 18:16:37 +0200

    fix next and prev buttons

--- HW3/js/script.js
+++ HW3/js/script.js
@@ -1,59 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
-  let slider__img = Array.from(document.querySelector(".slider__img").children);
-  let slider__item = Array.from(document.querySelectorAll(".data-more"));
+  let slider__img = document.querySelector(".slider__img").children;
+  let slider__item = Array.from(document.querySelectorAll(".data-more")); // Просто заюзал посмотреть в чём разница
   let buttom_prev = document.querySelector("#prev");
   let buttom_next = document.querySelector("#next");
 
-  
-  let i = 0;
-  buttom_next.addEventListener("click", () => {
-    if(i >= slider__img.length) {
-      i = 0;
-    }
-    if(slider__img[i].classList == "slider__show") {
-      slider__img[i].classList.replace("slider__show", "slider__hidden");
-      if(slider__img[i+1] == undefined){
-        slider__img[i].classList.replace("slider__show", "slider__hidden");
-        slider__img[0].classList.replace("slider__hidden", "slider__show");
-      } else {
-        slider__img[i+1].classList.replace("slider__hidden", "slider__show");
-      }
-    }
-    if(slider__item[i].classList == "data-more") {
-      slider__item[i].classList.add("hidden");
+    
+  let item_length = slider__item.length;
+  let item = 0;
+  function nextItem() {
+    slider__img[item].classList.toggle("hidden");
+    slider__item[item].classList.toggle("hidden");
 
-      if(slider__item[i+1] != undefined) {
-        slider__item[i+1].classList.remove("hidden");
-      } else {
-        slider__item[0].classList.remove("hidden");
-      }
-    } 
-    i++;
-  });
+    item = (item + 1) % item_length;
 
-  buttom_prev.addEventListener("click", () => {
-    if(slider__img[i-1] == undefined) {
-      slider__img[slider__img.length - 1].classList.replace("slider__hidden", "slider__show");
-      slider__img[i].classList.replace("slider__show", "slider__hidden");
-    } else if(slider__img[i].classList == "slider__show") {
-      slider__img[i].classList.replace("slider__show", "slider__hidden"); 
-      slider__img[i-1].classList.replace("slider__hidden", "slider__show"); 
-    }
-    if(slider__item[i].classList == "data-more") {
-      slider__item[i].classList.add("hidden");
-      if(slider__img[i-1] == undefined) {
-        slider__item[slider__img.length - 1].classList.remove("hidden");
-      } else {
-        slider__item[i-1].classList.remove("hidden");
-      }
-    } 
+    slider__img[item].classList.toggle("hidden");
+    slider__item[item].classList.toggle("hidden");
+  }
 
-    if(i <= 0) {
-      i = slider__img.length - 1;
-    } else {
-      i--;
-    }
-  });
+  function prevSlider() {
+    slider__img[item].classList.toggle("hidden");
+    slider__item[item].classList.toggle("hidden");
 
+    item = (item - 1 + item_length) % item_length;
+
+    slider__img[item].classList.toggle("hidden");
+    slider__item[item].classList.toggle("hidden");
+  }
+
+  buttom_next.addEventListener("click", nextItem);
+  buttom_prev.addEventListener("click", prevSlider);
 });
