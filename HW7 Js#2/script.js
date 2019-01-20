a882dfdba15932ff9dbf45dbbf1faca8131f4d97
IlyaBielov <ibioriginalibi@gmail.com>
2019-01-20 13:57:45 +0200

    finish

--- HW7 Js#2/script.js
+++ HW7 Js#2/script.js
@@ -1,25 +1,49 @@
-//level 1
-  while(isFree('north')){
-    north();
-  }
-
-//level 2
-  for (let i = 0; i < 16; i++){
-    east();
-  }
-
-//level 3-4
-  while(isFree('south') || isFree('east')){
-    if (isFree('south')) {
-      south();
-    } else if( isFree('east')) {
-      east();
+//All levels
+function nextLevel() {
+    let finish = null;
+    let before = '';
+    while (finish !== "next") {
+        if (isFree('west') && before !== 'east') {
+            if (isFree('west') && isFree('east')) {
+                while (isFree('east')) {
+                    finish = east();
+                    before = 'east';
+                }
+            } else {
+                while (isFree('west')) {
+                    finish = west();
+                    before = 'west';
+                }
+            }
+        } else if (isFree('south') && before !== 'north') {
+            if (isFree('south') && isFree('north')) {
+                while (isFree('north')) {
+                    finish = north();
+                    before = 'north';
+                }
+            } else {
+                while (isFree('south')) {
+                    finish = south();
+                    before = 'south';
+                }
+            }
+        } else if (isFree('east') && before !== 'west') {
+            while (isFree('east')) {
+                finish = east();
+                before = 'east';
+            }
+        } else if (isFree('north')) {
+            if (!isFree('west') && !isFree('south') && !isFree('east')) {
+                finish = north();
+                before = 'north';
+            } else {
+                while (isFree('north')) {
+                    finish = north();
+                    before = 'north';
+                }
+            }
+        }
     }
-  }
-
-//level 5
-  // kill me please
-//level 6
-  // kill me please
-//level 7
-  // kill me please
+    return finish;
+};
+nextLevel();
