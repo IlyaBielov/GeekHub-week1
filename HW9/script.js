d9444cc98571f21e9b2d7143a38765fd7ac21758
IlyaBielov <ibioriginalibi@gmail.com>
2019-01-30 18:53:22 +0200

    use eslint

--- HW9/script.js
+++ HW9/script.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+/* eslint-disable func-names */
+document.addEventListener('DOMContentLoaded', () => {
     function getRandomNumberFirst() {
-        let num = (Math.random() * 10 + 1).toFixed();
+        const num = (Math.random() * 10 + 1).toFixed();
 
         setTimeout(() => {
             console.info(`Posts: ${num}`);
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function getRandomNumberSecond() {
-        let num = (Math.random() * 10/2 + 1).toFixed() ;
+        const num = (Math.random() * 10 / 2 + 1).toFixed();
 
         setTimeout(() => {
             console.info(`Page: ${num}`);
@@ -19,20 +20,20 @@ document.addEventListener("DOMContentLoaded", () => {
         return num;
     }
 
-    let limitNumber = getRandomNumberFirst();
-    let pageNumber = getRandomNumberSecond();
+    const limitNumber = getRandomNumberFirst();
+    const pageNumber = getRandomNumberSecond();
 
     function getByCallBack() {
         function getPost(callback) {
-            let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
                 if (this.readyState === 4 && this.status === 200) {
                     callback(this.responseText);
                 }
             };
-            xhttp.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
+            xhttp.open('GET', `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
             xhttp.send();
-        };
+        }
         getPost((responseText) => {
             console.log(JSON.parse(responseText));
         });
@@ -40,26 +41,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function getByPromise() {
         return new Promise((resolve, reject) => {
-            let xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
+            const xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
                 if (this.readyState === 4 && this.status === 200) {
                     resolve(this.responseText);
                 }
             };
-            xhttp.onload = function() {
+            xhttp.onload = function () {
                 if (this.status !== 200) {
                     reject(this.status);
                 }
             };
-            xhttp.open("GET", `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
+            xhttp.open('GET', `http://apistaging.theatre.pp.ua/posts.json?limit=${limitNumber}&page=${pageNumber}`);
             xhttp.send();
         });
     }
 
     async function getByAsync() {
-        return await getByPromise();
+        const promise = await getByPromise();
+        return promise;
     }
-    
 
     getByCallBack();
 
