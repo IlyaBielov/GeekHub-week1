eef659beae7633b752aa36112fc0794194d183d2
IlyaBielov <ibioriginalibi@gmail.com>
2019-02-04 11:38:24 +0200

    redone first step

--- HW11/src/js/script.js
+++ HW11/src/js/script.js
@@ -1,87 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const headerIpnut = document.querySelector(".header__ipnut");
+    const headerInput = document.querySelector('.header__ipnut');
+    const header__button = document.querySelector('.header__button');
+
     const list = document.querySelector('.list');
-    const headerButton = document.querySelector('.header__button');
+    const form = document.querySelector('.list__form');
 
-    class ListForm {
-        /**
-         * @param {string} nameClass 
-         * @param  {...Node} elements 
-         */
-        constructor(nameClass, ...elements) {
-            this._form = document.createElement('form');
-            this._form.classList.add(nameClass);
+    function out(form) {
+        const newForm = form.cloneNode(true);
+        newForm.classList.remove('none');
+        newForm.firstElementChild.value = headerInput.value;
 
-            if (elements.length > 0) {
-                for (const elem of elements) {
-                    this._form.appendChild(elem);
-                }
-            }
+        if (headerInput.value !== "") {
+            list.appendChild(newForm);
         }
-
-        get getForm() {
-            return this._form;
-        }
+        
+        headerInput.value = "";
     }
-
-    class ElemForm {
-        /**
-         * @param {string} className 
-         * @param {string} type 
-         * @param {string} value 
-         */
-        constructor(className, type, value = '') {
-            this._elemForm = document.createElement('input');
-            this._elemForm.classList.add(className);
-            this._elemForm.setAttribute('type', type);
-            this._elemForm.setAttribute('value', value);
-            this._elemForm.setAttribute('name', className);
-
-            if (type === 'button') {
-                this._elemForm.addEventListener('click', () => {
-                   this._elemForm.parentElement.remove();
-                });
-            }
-
-            if (type === 'checkbox') {
-                this._elemForm.addEventListener('click', () => {
-                    const listTask = this._elemForm.parentElement.querySelector('.list__task');
-                    if (listTask.disabled !== true) {
-                        listTask.disabled = true;
-                    } else if(listTask.disabled === true) {
-                        listTask.disabled = false;
-                    }
-                });
-            }
-        }
-
-        get getElem() {
-            return this._elemForm;
-        }
-    }
-
-    headerButton.addEventListener('click', function () {
-        if (headerIpnut.value !== "") {
-            const checkbox = new ElemForm('list__checkbox', 'checkbox');
-            const button = new ElemForm('list__button', 'button', '⨯');
-            const input = new ElemForm('list__task', 'text', headerIpnut.value);
-            const form = new ListForm("list__form", input.getElem, button.getElem, checkbox.getElem);
-            list.appendChild(form.getForm);
-            headerIpnut.value = "";
-        }
+    
+    header__button.addEventListener('click', function () {
+        out(form);        
     });
 
-    headerIpnut.addEventListener('keypress', function (e) {
-        if (headerIpnut.value !== "") {
-            if (e.key === 'Enter') {
-                const checkbox = new ElemForm('list__checkbox', 'checkbox');
-                const button = new ElemForm('list__button', 'button', '⨯');
-                const input = new ElemForm('list__task', 'text', headerIpnut.value);
-                const form = new ListForm("list__form", input.getElem, button.getElem, checkbox.getElem);
-                list.appendChild(form.getForm);
-                headerIpnut.value = "";
-            }
+    headerInput.addEventListener('keypress', function (e) {
+        if (e.key === 'Enter') {
+            out(form);
         }
     });
 });
