e89c71b1c41aa33ef46648a9fcae092d65673ef7
IlyaBielov <ibioriginalibi@gmail.com>
2019-02-05 17:34:45 +0200

    add eslint

--- HW11/src/js/script.js
+++ HW11/src/js/script.js
@@ -1,45 +1,40 @@
+// @ts-nocheck
 document.addEventListener('DOMContentLoaded', () => {
-
     const headerInput = document.querySelector('.header__ipnut');
-    const header__button = document.querySelector('.header__button');
+    const headerButton = document.querySelector('.header__button');
 
     const list = document.querySelector('.list');
-    const item = document.querySelector('.list__form');
+    const form = document.querySelector('.list__form');
 
-    header__button.addEventListener('click', function () {
-        if (headerInput.value !== "") {
-            
-            createTask(item);
-
-            headerInput.value = "";
-        } else {
-            alert('Enter your task');
-        }
-    });
-
-    /**
-    * @param {Element} item
-    */
     function createTask(item) {
         const newItem = item.cloneNode(true);
 
         newItem.classList.remove('none');
         newItem.querySelector('.list__task').value = headerInput.value;
 
-        newItem.querySelector('.list__checkbox').addEventListener('click', function () {
+        newItem.querySelector('.list__checkbox').addEventListener('click', () => {
             if (newItem.querySelector('.list__task').disabled !== true) {
                 newItem.querySelector('.list__task').disabled = true;
             } else {
                 newItem.querySelector('.list__task').disabled = false;
             }
-
         });
 
-        newItem.querySelector('.list__button').addEventListener('click', function () {
+        newItem.querySelector('.list__button').addEventListener('click', () => {
             newItem.remove();
         });
 
         list.appendChild(newItem);
         return newItem;
     }
+
+    headerButton.addEventListener('click', () => {
+        if (headerInput.value !== '') {
+            createTask(form);
+
+            headerInput.value = '';
+        } else {
+            console.warn('Enter your task');
+        }
+    });
 });
