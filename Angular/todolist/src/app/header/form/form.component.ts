2a3c753c1c93b428724448eeb8b041412c10487f
IlyaBielov <ibioriginalibi@gmail.com>
2019-03-13 09:42:52 +0200

    add id for task

--- Angular/todolist/src/app/header/form/form.component.ts
+++ Angular/todolist/src/app/header/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoListService } from 'src/app/todo-list.service';
+import { IdService } from 'src/app/id.service';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +10,14 @@ import { TodoListService } from 'src/app/todo-list.service';
 export class FormComponent {
   inputValue: string;
 
-  constructor(private todoListService: TodoListService) {
+  constructor(private todoListService: TodoListService, private idService: IdService) {
   }
 
   addTask(): void {
     if (!this.inputValue) { return; }
 
     this.todoListService.todoList.push({
+      id: this.idService.id,
       text: this.inputValue,
       isChecked: false,
       isDeleted: false
